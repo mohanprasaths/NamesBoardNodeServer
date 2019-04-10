67db3b7095e0a926fa6811bddce460d768dc0913
moprasat <moprasat@cisco.com>
2019-04-10 20:38:42 +0530

    fix cors

--- main.js
+++ main.js
@@ -8,6 +8,12 @@ var count = 0;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 var names=[]
 var registerEventHandler =  function(name,mailid,password){
 	console.log(name)
